42e5d5e2b67e14f1a5af043641c0636c063ab6a7
cyborgEneki <joanreneki@gmail.com>
2023-03-30 19:25:19 +0300

    Add router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// Bootstrap CSS
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import App from './App';
+import ErrorPage from './components/ErrorPage';
+import JokeDisplay from './components/JokeDisplay';
 
-import App from './App'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/random-joke/:category",
+    element: <JokeDisplay />
+  }
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
